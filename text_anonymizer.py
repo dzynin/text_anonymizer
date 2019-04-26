cb042489d3bc0fbd1837bd0449b36c204d494e5d
Aras Kayvanrad <mkayvan@gmail.com>
2019-04-26 16:49:42 -0400

    select name types to annonymize

--- text_anonymizer.py
+++ text_anonymizer.py
@@ -21,14 +21,70 @@ To do:
 
 import en_core_web_sm
 
-def replace_name_with_placeholder(token):
-    if token.ent_iob != 0 and token.ent_type_ == "PERSON":
+def replace_name_with_placeholder(token, name_types=["PERSON"]):
+    '''
+    For a given token, return the token text or a place holder depending on the
+    token type.
+    Inputs:
+        token: spaCy token object
+        name_types: name types to be annonymized (default = "PERSON")
+                    valid types include:
+                        PERSON	People, including fictional.
+                        NORP	Nationalities or religious or political groups.
+                        FAC	Buildings, airports, highways, bridges, etc.
+                        ORG	Companies, agencies, institutions, etc.
+                        GPE	Countries, cities, states.
+                        LOC	Non-GPE locations, mountain ranges, bodies of water.
+                        PRODUCT	Objects, vehicles, foods, etc. (Not services.)
+                        EVENT	Named hurricanes, battles, wars, sports events, etc.
+                        WORK_OF_ART	Titles of books, songs, etc.
+                        LAW	Named documents made into laws.
+                        LANGUAGE	Any named language.
+                        DATE	Absolute or relative dates or periods.
+                        TIME	Times smaller than a day.
+                        PERCENT	Percentage, including ”%“.
+                        MONEY	Monetary values, including unit.
+                        QUANTITY	Measurements, as of weight or distance.
+                        ORDINAL	“first”, “second”, etc.
+                        CARDINAL	Numerals that do not fall under another type.
+    Outputs:
+        text: either the origin token text or a place holder
+    '''
+    if token.ent_iob != 0 and token.ent_type_ in name_types:
         txt = "[REDACTED]"
     else:
         txt = token.text
     return txt + token.whitespace_
 
-def anonymize_placeholder(text):
+
+def anonymize_placeholder(text, name_types=["PERSON"]):
+    '''
+    Annonymize a piece of text by replacing names with a place holder.
+    Inputs:
+        text: input text
+        name_types: name types to be annonymized (default = "PERSON")
+                    valid types include:
+                        PERSON	People, including fictional.
+                        NORP	Nationalities or religious or political groups.
+                        FAC	Buildings, airports, highways, bridges, etc.
+                        ORG	Companies, agencies, institutions, etc.
+                        GPE	Countries, cities, states.
+                        LOC	Non-GPE locations, mountain ranges, bodies of water.
+                        PRODUCT	Objects, vehicles, foods, etc. (Not services.)
+                        EVENT	Named hurricanes, battles, wars, sports events, etc.
+                        WORK_OF_ART	Titles of books, songs, etc.
+                        LAW	Named documents made into laws.
+                        LANGUAGE	Any named language.
+                        DATE	Absolute or relative dates or periods.
+                        TIME	Times smaller than a day.
+                        PERCENT	Percentage, including ”%“.
+                        MONEY	Monetary values, including unit.
+                        QUANTITY	Measurements, as of weight or distance.
+                        ORDINAL	“first”, “second”, etc.
+                        CARDINAL	Numerals that do not fall under another type.
+    Outputs:
+        text: annonymized text
+    '''
     nlp = en_core_web_sm.load()
     
     # parse the text with spacy
@@ -36,5 +92,5 @@ def anonymize_placeholder(text):
     
     for ent in doc.ents:
         ent.merge()
-    tokens = map(replace_name_with_placeholder, doc)
-    return " ".join(tokens) 
+    tokens = [replace_name_with_placeholder(t, name_types=name_types) for t in doc]
+    return "".join(tokens) 
