fc8905011ed903c149bf32dd636f2b5aef51f02b
Aras Kayvanrad <mkayvan@gmail.com>
2020-01-22 10:24:37 -0500

    webapp.py - add api function

--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import text_anonymizer
+import json
 
 app = Flask(__name__)
 
@@ -15,10 +16,34 @@ def my_form_post():
     name_types = request.form.getlist('name_types_checkbox')
     fictional_checkbox = request.form.getlist('fictional_checkbox')
     fictional = "FICTIONAL" in fictional_checkbox
-    anonymized_text = text_anonymizer.anonymize(text, name_types=name_types, fictional = fictional)
+    anonymized_text = text_anonymizer.anonymize(text,
+                                                name_types=name_types,
+                                                fictional = fictional)
     return render_template('form.html',
                            original_text = text,
                            anonymized_text = anonymized_text)
+
+
+@app.route('/api', methods=['POST'])
+def my_api_post():
+    # input data in json format
+    input_json = request.json
+
+    # parse json data
+    d = json.loads(input_json)
+
+    # anonymize text
+    anonymized_text = text_anonymizer.anonymize(d['text'],
+                                                name_types=d['entities'],
+                                                fictional = d['fake_names'])
+    
+    # convert the output into a json file
+    response = json.dumps(anonymized_text)
+
+    # return the json file
+    return response
+
+
 
 
 if __name__ == '__main__':
